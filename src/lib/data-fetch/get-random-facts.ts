3df406e5b2c7550b6d2168aa80a7d88e3685a60a
bluepnwage <a.carty2555@gmail.com>
2024-07-25 09:59:23 -0400

    remove nextCache for random articles

--- src/lib/data-fetch/get-random-facts.ts
+++ src/lib/data-fetch/get-random-facts.ts
@@ -1,9 +1,7 @@
 import { createClient } from "../supabase/client";
-import { unstable_cache as nextCache } from "next/cache";
 
-
-export const getRandomFacts = nextCache(async () => {
-    const { data, error } = await createClient().from('random_facts').select('*');
-    if (error) throw new Error('Failed to fetch data');
-    return data
-})
+export const getRandomFacts = async () => {
+  const { data, error } = await createClient().from("random_facts").select("*");
+  if (error) throw new Error("Failed to fetch data");
+  return data;
+};
